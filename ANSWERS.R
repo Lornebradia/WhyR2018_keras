b31bdbe5d7613fe6f43af547afad091dfec9a616
Michał <michalmaj116@gmail.com>
2018-06-30 22:13:15 +0200

    convnet answers part 1

--- ANSWERS.R
+++ ANSWERS.R
@@ -30,3 +30,35 @@ fashion_model %>% evaluate(fashion_mnist_test_X, fashion_mnist_test_Y)
 fashion_predictions <- fashion_model %>% predict(fashion_mnist_test_X)
 
 # Ex. 2.
+ships_train$labels <- ships_train$labels %>% to_categorical(., 2)
+ships_test$labels <- ships_test$labels %>% to_categorical(., 2)
+
+ships_model <- keras_model_sequential() %>%
+  layer_conv_2d(
+    input_shape = c(80, 80, 3),
+    filter = 32, kernel_size = c(3, 3), strides = c(1, 1),
+    activation = "relu")
+
+ships_model %>%
+  layer_max_pooling_2d(pool_size = c(2, 2), strides = c(2, 2)) %>%
+  layer_conv_2d(filter = 64, kernel_size = c(3, 3), strides = c(1, 1),
+                activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2, 2), strides = c(2, 2)) %>%
+  layer_flatten() %>%
+  layer_dense(2, activation = "softmax")
+
+ships_model %>% compile(
+  loss = "binary_crossentropy",
+  optimizer = optimizer_sgd(lr = 0.0001, decay = 1e-6),
+  metrics = "accuracy"
+)
+
+ships_fit <- ships_model %>% fit(ships_train$data, ships_train$labels,
+                                 epochs = 20, batch_size = 32,
+                                 validation_split = 0.2)
+
+ships_model %>% evaluate(ships_test$data, ships_test$labels)
+
+save_model_hdf5(ships_model, "ships_model.hdf5")
+
+# Ex. 3.
--- ConvNet.R
+++ ConvNet.R
@@ -15,16 +15,12 @@ plot_sample_image(ships_train$data, ships_train$labels, show_layers = TRUE, row_
 
 # Ex. 2 - Build a simple ConvNet for binary classification
 # 1. Change labels vectors to one-hot-encoding matrix
-ships_train$labels <- ships_train$labels %>% to_categorical(., 2)
-ships_test$labels <- ships_test$labels %>% to_categorical(., 2)
+ships_train$labels <- 
+ships_test$labels <- 
 
 # 2. Initialize sequential model and add 2d convolutional layer
 # with 32 filters, 3x3 kernel, 1x1 stride, "relu" activation
-ships_model <- keras_model_sequential() %>%
-  layer_conv_2d(
-    input_shape = c(80, 80, 3),
-    filter = 32, kernel_size = c(3, 3), strides = c(1, 1),
-    activation = "relu")
+ships_model <- 
 summary(ships_model)
 
 # 3. Explain output shape and nr of params
@@ -35,33 +31,21 @@ summary(ships_model)
 # max pooling with 2x2 pool (kernel), 2x2 strides
 # flattening layer
 # Dense layer as output
-ships_model %>%
-  layer_max_pooling_2d(pool_size = c(2, 2), strides = c(2, 2)) %>%
-  layer_conv_2d(filter = 64, kernel_size = c(3, 3), strides = c(1, 1),
-                activation = "relu") %>%
-  layer_max_pooling_2d(pool_size = c(2, 2), strides = c(2, 2)) %>%
-  layer_flatten() %>%
-  layer_dense(2, activation = "softmax")
+
 summary(ships_model)
 
 # 5. Compile the model using binary crossentropy as loss function and
 # SGD as optimizer with learning rate equal to 0.0001 and learning rate decay equal to 1e-6
-ships_model %>% compile(
-  loss = "binary_crossentropy",
-  optimizer = optimizer_sgd(lr = 0.0001, decay = 1e-6),
-  metrics = "accuracy"
-)
+
 
 # 6. Fit the model. Use 20% of the data for validation, 20 epochs and 32 samples for batch size.
-ships_fit <- ships_model %>% fit(ships_train$data, ships_train$labels,
-                             epochs = 20, batch_size = 32,
-                             validation_split = 0.2)
+ships_fit <- 
 
 # 7. Evaluate model on test set
-ships_model %>% evaluate(ships_test$data, ships_test$labels)
+  
 
 # 8. Save model in hdf5 format
-save_model_hdf5(ships_model, "ships_model.hdf5")
+
 
 # Ex 3. Build second model using batch normalization. Use early stopping and checkpoints. Save logs to Tensorboard
 # 1. Model architecture:
